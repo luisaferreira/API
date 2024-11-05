95736afceda1d3dea7804990efe72bc9b25efa99
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2024-11-05 19:55:08 -0300

    Adicionado o projeto

---
+++ MusicaAPI/Controllers/MusicaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MusicaAPI.Models;
+
+namespace MusicaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicaController : Controller
+    {
+        private List<Musica> musicas;
+
+        public MusicaController()
+        {
+            musicas = new List<Musica>()
+            {
+                new Musica("$20", "boygenius", "Indie rock", new DateTime(2023, 3, 31), "the record", "Interscope Records"),
+                new Musica("Emily I'm Sorry", "boygenius", "Indie rock", new DateTime(2023, 3, 31), "the record", "Interscope Records"),
+                new Musica("True Blue", "boygenius", "Indie rock", new DateTime(2023, 3, 31), "the record", "Interscope Records"),
+            };
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Obter()
+        {
+            return Ok(musicas);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Inserir(Musica musica)
+        {
+            musicas.Add(musica);
+
+            return Created();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# MusicaAPI
+API de música desenvolvida para a matéria de Gestão da Configuração II.
+
+## Como executar o projeto?
+**Necessário ter o .Net 8 instalado em sua máquina**
+### No Visual Studio
+- Clone este repositório.
+- Abra o projeto na IDE e pressione F5 para executar.
+### No Visual Studio Code
+- Clone este repositório
+- Abra o projeto no editor de código e no terminal execute o comando _dotnet run_
+
+Após executar o projeto, utilize ferramentas como o Swagger ou o Postman para acessar os endpoints.
+
+## Workflow
+Tendo em vista que o projeto é uma API simples administrada por uma única pessoa o fluxo de trabalho escolhido foi o Github Flow.
